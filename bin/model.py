b6a5f3f75a649529bd8b051dd093e23eb8b27197
19746 <mattiabraga2006@gmail.com>
2024-10-29 18:46:26 +0100

    Added data structure

--- bin/model.py
+++ bin/model.py
@@ -100,9 +100,12 @@ def inference():
     """
 
     additional_instructions = """
-    - If you don't know the answer to a question, it's okay to say so, and andvise the student to ask a teacher or a school administrator.
-    - Inform that you are still learning and improving, and you might not have all the answers.
+- If you don't know the answer to a question, it's okay to say so, and andvise the student to ask a teacher or a school administrator.
+- Inform that you are still learning and improving, and you might not have all the answers.
     """
+    
+    print(additional_instructions)
+    return
     
     
     m = Model(GROQ_API_KEY, language="italian", school_info=school_info, additional_instructions=additional_instructions)
